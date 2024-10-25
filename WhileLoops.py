aab6095f6f22108326db3b8c028cfb2872d9a89b
Olivia506 <omh7@kent.ac.uk>
2024-10-25 12:05:46 +0000

    d

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,12 @@
 ### Part Two -- your code goes here. 
+import random
+random.randint(1, 100)
+while True:
+    number = input("guess the number.")
+    if number < random.randint: 
+        continue
+    elif number > random.randint:
+        continue
+    else:
+        break
+
--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,6 @@
 ### Part One -- your code goes here. 
+for number in range (1, 11):
+    square = number ** 2
+    print (f"the square of {number} is {square}")
+  
+
